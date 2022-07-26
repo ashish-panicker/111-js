7ad26db99a194abd94cd5f1ce6f62ca9e0ad2a7a
ashish-panicker <ashish.s.panicker@outlook.com>
2022-07-26 16:04:48 +0530

    Response entity and status codes

--- java/getting-started-spring-data-jpa/src/main/java/in/stackroute/gettingstartedspringdatajpa/controllers/MentorController.java
+++ java/getting-started-spring-data-jpa/src/main/java/in/stackroute/gettingstartedspringdatajpa/controllers/MentorController.java
@@ -1,14 +1,18 @@
 package in.stackroute.gettingstartedspringdatajpa.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.validation.Valid;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import in.stackroute.gettingstartedspringdatajpa.domain.Address;
 import in.stackroute.gettingstartedspringdatajpa.domain.Mentor;
 import in.stackroute.gettingstartedspringdatajpa.dto.MentorDto;
 import in.stackroute.gettingstartedspringdatajpa.service.MentorService;
@@ -26,27 +30,45 @@ public class MentorController {
     }
 
     // Post request to create a new mentor
+    // on successful creation of mentor, return the created mentor with status code 201
     @PostMapping("/mentors")
-    public Mentor createMentor(@RequestBody @Valid MentorDto mentorDto) {
-        Mentor mentor = getMentorInstance(mentorDto);
-        return mentorService.create(mentor);
+    public ResponseEntity<MentorDto> createMentor(@RequestBody @Valid MentorDto mentorDto) {
+        Mentor mentor = modelMapper.map(mentorDto, Mentor.class);
+        mentor = mentorService.create(mentor);
+        mentorDto = modelMapper.map(mentor, MentorDto.class);
+        return ResponseEntity.status(HttpStatus.CREATED).body(mentorDto);
     }
 
     // Get request to fetch all mentors
     @GetMapping("/mentors")
-    public List<Mentor> getAllMentors() {
-        return mentorService.getAllMentors();
+    public ResponseEntity<Map<Object, Object>> getAllMentors() {
+        Map<Object, Object> response = new HashMap<>();
+        var mentors = mentorService.getAllMentors();
+        if (mentors.isEmpty()) {
+            response.put("message", "No mentors found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+        }
+        response.put("status", HttpStatus.OK);
+        response.put("size", mentors.size());
+        response.put("mentors", mentors);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     // Get request to fetch a mentor by id
     // /mentors/10
     @GetMapping("/mentors/{id}")
-    public Mentor getMentorById(@PathVariable int id) {
-        return mentorService.getMentorById(id).orElseThrow();
+    public ResponseEntity<MentorDto> getMentorById(@PathVariable int id) {
+        var mentorDto = modelMapper.map(mentorService.getMentorById(id).orElseThrow(), MentorDto.class);
+        return ResponseEntity.status(HttpStatus.OK).body(mentorDto);
     }
 
-    private Mentor getMentorInstance(MentorDto mentorDto) {
-        return modelMapper.map(mentorDto, Mentor.class);
+    // Delete request to delete a mentor by id
+    // /mentors/10
+    @DeleteMapping("/mentors/{id}")
+    public ResponseEntity<Map<String, String>> deleteMentorById(@PathVariable int id) {
+        mentorService.delete(mentorService.getMentorById(id).orElseThrow());
+        Map<String, String> response = Map.of("message", "Mentor deleted successfully", "status",
+                HttpStatus.OK.toString());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
-
 }
